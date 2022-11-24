d849aa28d807b70b895fbf2abf9c13b2f0d67bfc
junweilam <91267859+junweilam@users.noreply.github.com>
2022-11-24 13:47:56 +0800

    Added charts for accelerometer and ultrasonic

---
+++ Ultrasonic_Accelerometer/Charts/Accelerometer/accelerometer.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+xu,yu = [], []
+xk,yk = [], []
+xs,ys = [], []
+
+for line in open('X1Unfiltered.txt', 'r'):
+    values = [float(s) for s in line.split()]
+    print(values[0])
+    yu.append(values[0])
+    xu.append(values[1])
+
+for line in open('X1KalmanFilter.txt', 'r'):
+    values = [float(s) for s in line.split()]
+    yk.append(values[0])
+    xk.append(values[1])
+
+for line in open('X1SMA.txt', 'r'):
+    values = [float(s) for s in line.split()]
+    ys.append(values[0])
+    xs.append(values[1])
+
+plt.plot(xu,yu, c = 'r', label="Unfiltered Data")
+plt.plot(xk, yk, c = 'tab:blue', label="Kalman Filter Data")
+plt.plot(xs, ys, c = 'tab:green', label = 'Simple Moving Average Data')
+plt.title(label = "Accelerometer X degree Algorithm Comparison", fontweight = "bold")
+plt.xlabel("nth digit")
+plt.ylabel("x-  Degree")
+plt.legend()
+plt.show()
